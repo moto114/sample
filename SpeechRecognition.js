b2ec33a5b309e4c795e6ad17a783adb263733d22
moto114 <42561840+moto114@users.noreply.github.com>
2020-06-04 13:41:37 +0900

    Update SpeechRecognition.js

--- SpeechRecognition.js
+++ SpeechRecognition.js
@@ -1,6 +1,7 @@
 const startBtn = document.querySelector('#start-btn');
 const stopBtn = document.querySelector('#stop-btn');
 const resultDiv = document.querySelector('#result-div');
+const logDiv = document.querySelector('#log-div');
 
 SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
 let recognition = new SpeechRecognition();
@@ -9,18 +10,24 @@ recognition.interimResults = true;
 recognition.continuous = true;
 
 let finalTranscript = ''; // 確定した(黒の)認識結果
+let logTranscript = '';
 
 recognition.onresult = (event) => {
-let interimTranscript = ''; // 暫定(灰色)の認識結果
-for (let i = event.resultIndex; i < event.results.length; i++) {
-  let transcript = event.results[i][0].transcript;
-  if (event.results[i].isFinal) {
-    finalTranscript += transcript;
-  } else {
-    interimTranscript = transcript;
-  }
-}
-resultDiv.innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</i>';
+	let interimTranscript = ''; // 暫定(灰色)の認識結果
+	for (let i = event.resultIndex; i < event.results.length; i++) {
+	  let transcript = event.results[i][0].transcript;
+	  if (event.results[i].isFinal) {
+	    //finalTranscript += transcript;
+	    finalTranscript = transcript;
+		logTranscript += "<br>" + transcript;
+
+	  } else {
+	    interimTranscript = transcript;
+
+	  }
+	}
+	resultDiv.innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</i>';
+	logDiv.innerHTML = logTranscript;
 }
 
 startBtn.onclick = () =>{
